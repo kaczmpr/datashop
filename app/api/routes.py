1626e2c4a42def6ceb01ca8f460c768f09a30ed4
kaczmpr <przemek10014@gmail.com>
2021-02-01 17:48:44 +0100

    Added shop route in api

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,9 +1,15 @@
 from flask import jsonify, render_template, url_for, request, current_app
 from app.api import bp
-from app.models import Product
+from app.models import Product, Shop
 
 # Get All Products
 @bp.route('/product', methods=['GET'])
 def get_products():
     all_products = Product.query.all()
     return jsonify(all_products)
+
+
+@bp.route('/shop', methods=['GET'])
+def get_shops():
+    all_shops = Shop.query.all()
+    return jsonify(all_shops)
