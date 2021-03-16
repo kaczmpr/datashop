4310b8fe5d01d134009c5de0aecd488f52d7776b
kaczmpr <61713782+kaczmpr@users.noreply.github.com>
2021-03-16 19:52:06 +0100

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-class Config(object):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'snowflake://admin:Datashop1!@uu39760.west-europe.azure/DATASHOP_DB/DEVELOP?warehouse=COMPUTE_WH&role=SYSADMIN'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
