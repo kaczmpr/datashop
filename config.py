e1ae460c38cc651fabfa02933eac44900212bb0c
kaczmpr <61713782+kaczmpr@users.noreply.github.com>
2021-03-16 19:55:26 +0100

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-class Config(object):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'snowflake://'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_key'
